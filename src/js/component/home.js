c4b41025e1a9875639ce1020d99faa860920919e
achaves100 <adan.chaves@outlook.com>
2021-04-03 02:43:01 +0000

    SetTask ready

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function Home() {
+	const [task, setTask] = useState("");
+	const [list, setList] = useState([]);
+
 	return (
-		<div className="container ">
+		<div className="container text-muted">
 			<h1 className="text-lg-center">To Do List</h1>
 			<div className="input-group mb-3">
 				<div className="input-group-prepend"></div>
 				<input
+					onChange={e => setTask(e.target.value)}
+					value={task}
+					onKeyPress={e => {
+						if (e.key == "Enter") {
+							setList(list.concat(task));
+							setTask("");
+							console.log({ list });
+						}
+					}}
 					type="text"
 					className="form-control"
 					placeholder=""
@@ -14,6 +26,9 @@ export function Home() {
 					aria-describedby="basic-addon1"
 				/>
 			</div>
+			<ul style={{ listStyleType: "none" }}>
+				<li></li>
+			</ul>
 		</div>
 	);
 }
